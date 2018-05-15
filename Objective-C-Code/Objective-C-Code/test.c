97081b410a9f9649d6921fd9187949ecc4afbaed
yanhangpu <sogou@sogoudeiMac.local>
2018-05-15 11:00:32 +0800

    add test file

---
+++ Objective-C-Code/Objective-C-Code/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  Objective-C-Code
+//
+//  Created by sogou-Yan on 2018/5/14.
+//  Copyright © 2018年 sogou. All rights reserved.
+//
+
+#include "test.h"
---
+++ Objective-C-Code/Objective-C-Code/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  Objective-C-Code
+//
+//  Created by sogou-Yan on 2018/5/14.
+//  Copyright © 2018年 sogou. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */
